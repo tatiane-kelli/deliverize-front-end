2ef236e6602128a480663f630744565477750152
Tatiane Kelli <tatianekelli.dev@gmail.com>
2022-03-13 14:36:51 -0300

    fixed: positions

---
+++ src/components/header/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./styles.css";
+import { BiUserCircle } from "react-icons/bi";
+import { MdOutlineLocalGroceryStore } from "react-icons/md";
+import { RiArrowDownSLine } from "react-icons/ri";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navigationHeader">
+        <div className="session__logo">
+          <img src="../../assets/deliverize-logo1.svg" alt="Deliverize"/>
+          <img src="../../assets/deliverize-logo2.svg" alt="Deliverize"/>
+        </div>
+        <div className="delivery__address">
+          <div>
+            <p style={{color: "#ed3237", paddingLeft: "15px"}}><b>Entrega</b></p>
+            <p style={{color: "#656363", paddingLeft: "15px"}}>R. Antônio Braune, 222</p>
+          </div>
+          <div style={{color: "#ed3237",
+                      paddingLeft: "50px",
+                      width: "20px",
+                      height:"14px"
+                      }}
+          >
+            <RiArrowDownSLine />
+          </div>
+          
+        </div>
+        <div>
+          <input type="text" placeholder="Busque por estabelecimentos ou produtos"/>
+        </div>
+        <div className="profile__icons">
+          <p style={{paddingRight: "25px", marginLeft: "0px"}}>
+            <BiUserCircle />
+            Entrar
+          </p>
+          <p>
+            <MdOutlineLocalGroceryStore />
+            Carrinho
+          </p>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Header from "../components/Header";
+import Header from "../components/header";
+//import {AiOutlineMinus, AiOutlinePlus} from "react-icons/ai";
 import "./styles.css";
 
 const Product = () => {
@@ -9,18 +10,20 @@ const Product = () => {
       <div className="container">
         <div className="snack__info">
           <img src="PicanhaBacon.svg" alt="Oferta Picanha Cheddar Bacon" />
-          <h3 style={{color: "#4E4E4E", fontSize: "28px", lineHeight: "28px"}}>
+          <h3>
             Oferta Picanha Cheddar Bacon
           </h3>
           <p style={{color: "#4E4E4E", fontSize: "20px", lineHeight: "24px"}}>
             Hambúrguer de picanha, molho de picanha, cebola crispy, bacon, <br/>
             queijo cheddar, molho cheddar e pão mix de gergelim.
           </p>
-          <div style={{display:"flex"}}>
-          <p style={{color: "#DC6E05", fontSize: "32px", lineHeight: "38.4px"}}>
+          <div id="prices">
+          <p style={{color: "#DC6E05"}}>
             R$31,99
           </p>
-          <p style={{color: "#4E4E4E", fontSize: "32px", lineHeight: "38.4px", paddingLeft: "15px"}}>
+          <p style={{color: "#4E4E4E",
+                    paddingLeft: "15px",
+                    textDecoration: "line-through"}}>
             R$34,99
           </p>
           </div>
@@ -28,11 +31,11 @@ const Product = () => {
         </div>
         <div className="additional__orders">
           <div className="retangle">
-            <p style={{color: "#4E4E4E", lineHeight: "16px", fontSize: "14px", fontWeight: "500"}}>
+            <p id="add__ingredients">
               Adicionar ingredientes
             </p>
-            <p style={{color: "#E49700", lineHeight: "20px", fontSize: "12px", fontWeight: "400"}}>
-              Até 8 ingredientes
+            <p id="max-ingredients">
+              Até 8 ingredientes.
             </p>
           </div>
           <div className="extra__ingredient">
@@ -56,14 +59,11 @@ const Product = () => {
           </div>
           <div className="yellow-line"/>
           <div className="retangle">
-            <p style={{color: "#4E4E4E", lineHeight: "16px", fontSize: "14px", fontWeight: "500"}}>
-              Precisa de talher?
+            <p id="cutlery">
+              Precisa de Talher?
             </p>
           </div>
-          <div>
-            <a href="/">+</a> 0 <a href="/">-</a>
-          </div>
-          <div>
+          <div className="board__footer">
             <button>Adicionar</button>
           </div>
         </div>
